14e2ec722b3f67312753309415a918cd0ebd4c8f
roymondChandra <roymond.chandra93@gmail.com>
2022-10-10 14:26:22 +0700

    Hero Separo

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
 import Header from "parts/Header";
+import Hero from "parts/Hero";
+
+import landingPage from "json/LandingPages.json";
 export default class LandingPages extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero}
       </>
     );
   }
